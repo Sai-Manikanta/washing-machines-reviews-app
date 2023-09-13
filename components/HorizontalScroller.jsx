ba32d55f39095a6c0f133295908bbba86115fd7e
Devi-Kiran <devikiran333007@gmail.com>
2023-09-13 10:01:53 +0530

    fixed scroller bug

--- components/HorizontalScroller.jsx
+++ components/HorizontalScroller.jsx
@@ -14,7 +14,7 @@ export default function HorizontalScroller({ children }) {
       <ScrollMenu
         LeftArrow={() => LeftArrow(isOnHover)}
         RightArrow={() => RightArrow(isOnHover)}
-        scrollContainerClassName="snap-x snap-mandatory-x pt-5 pb-3"
+        scrollContainerClassName="snap-x snap-mandatory-x lg:snap-none pt-5 pb-3"
       >
         {children}
       </ScrollMenu>
--- components/ProductCard.js
+++ components/ProductCard.js
@@ -24,9 +24,12 @@ const ProductCard = ({ id, washingMachineData }) => {
   } = washingMachineData;
   const router = useRouter();
   const url = router.asPath;
-  
+
   const [show, setShow] = useState(false);
 
+  const viewMoreDetailsHandler = () => {
+    setShow((prev) => !prev);
+  };
 
   useEffect(() => {
     if (url?.includes(`product-${id}`)) {
@@ -197,7 +200,7 @@ const ProductCard = ({ id, washingMachineData }) => {
         <div>
           <div className={`max-w-lg flex sm:justify-between items-center mt-4`}>
             <button
-              onClick={() => setShow((prev) => !prev)}
+              onClick={viewMoreDetailsHandler}
               className="flex flex-grow gap-x-2 cursor-none sm:cursor-pointer items-center"
             >
               <span className="text-sm w-[6rem] flex justify-between items-center text-slate-600 hover:text-slate-900">
