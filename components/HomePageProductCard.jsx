984cffaecde25c4ee2aa808b8182957089b8ed1b
Devi-Kiran <devikiran333007@gmail.com>
2023-11-08 18:40:40 +0530

    fixed spacing issue for small product card

--- components/HomePageProductCard.jsx
+++ components/HomePageProductCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 function HomePageProductCard({productImage, productAlt, productName}) {
   return (
-    <div className="w-32 h-[197px] sm:w-36 sm:h-56 p-2.5 bg-white sm:mr-3 rounded md:hover:scale-110 duration-300">
+    <div className="w-32 h-[197px] sm:w-36 sm:h-56 p-2.5 bg-white mr-2 sm:mr-3 rounded md:hover:scale-110 duration-300">
         <div className="flex justify-center">
         <Image className="mb-2.5 h-[120px] sm:h-[142px] w-auto object-cover" src={`${productImage}`}
           alt={`${productAlt}`}
